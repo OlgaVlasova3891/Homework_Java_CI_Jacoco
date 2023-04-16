d34191219305e889b36b62be27cad6088c0d8174
Olga Vlasova <olgav1918@gmail.com>
2023-04-16 05:40:37 +0300

    changed commit

--- src/main/java/ru/netology/javaqa/javamvnci/statistic/StatisticsService.java
+++ src/main/java/ru/netology/javaqa/javamvnci/statistic/StatisticsService.java
@@ -4,10 +4,11 @@ public class StatisticsService {
     public long findMax(long[] incomes) {
         long currentMax = incomes[0];
         for (long income : incomes) {
-            if (currentMax <= income) {
+            if (currentMax < income) {
                 currentMax = income;
             }
         }
         return currentMax;
+
     }
 }
